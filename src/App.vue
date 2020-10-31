f1b85829ded0d7c2f1eaaf978e307968b644148b
clf94-dev <clf94@hotmail.es>
2020-10-31 18:17:27 +0100

    component created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
+<div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Component1 />
+</div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Component1 from './components/Component1.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld,
+        Component1
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/Component1.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <hr>
+    <h1>This is component 1</h1>
+    <p>{{text}}</p>
+    <hr>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Component1",
+    data() {
+        return {
+            text: "this is some text"
+        }
+    },
+}
+</script>
