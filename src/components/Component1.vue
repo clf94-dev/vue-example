54d48d4f129a33b92d5ae9dad620e2964056326e
clf94-dev <clf94@hotmail.es>
2020-10-31 19:09:23 +0100

    component inside a component

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -4,12 +4,17 @@
     <h1>This is component 1</h1>
     <p>{{text}}</p>
     <hr>
+    <Component2></Component2>
 </div>
 </template>
 
 <script>
+import Component2 from './Component2.vue'
 export default {
     name: "Component1",
+    components: {
+        Component2
+    },
     data() {
         return {
             text: "this is some text"
